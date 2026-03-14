92d5a6d760821b7b20b6e6459582042c09ddb57c
z <20153866+zakashic@users.noreply.github.com>
2026-03-15 00:22:22 +0800

    fix playcover-nightly.rb

--- Casks/playcover-nightly.rb
+++ Casks/playcover-nightly.rb
@@ -2,19 +2,19 @@ cask "playcover-nightly" do
   version :latest
   sha256 :no_check
 
-  # 1. 指向 nightly.link 的展示页面
-  url "https://nightly.link/PlayCover/PlayCover/workflows/2.nightly_release/develop" do |page|
-    # 2. 使用正则表达式从页面源码中提取包含 "PlayCover_nightly" 字样且以 ".dmg.zip" 结尾的链接
-    file_path = page[%r{href="([^"]+PlayCover_nightly_[^"]+\.dmg\.zip)"}, 1]
-    
-    # 3. 拼接成完整的下载地址
-    "https://nightly.link#{file_path}" if file_path
+  url do
+    require "net/http"
+    require "uri"
+
+    workflow_url = "https://nightly.link/PlayCover/PlayCover/workflows/2.nightly_release/develop"
+    response = Net::HTTP.get_response(URI(workflow_url))
+    "#{response["location"]}.zip"
   end
-  
+
   name "PlayCover"
   desc "Sideload iOS apps and games"
   homepage "https://github.com/PlayCover/PlayCover"
-  
+
   auto_updates true
   conflicts_with cask: "playcover-community"
   depends_on arch: :arm64
