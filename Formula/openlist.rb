f37be676c5d84a045bcda6724eb2659d440e2b93
Andy L <20153866+zakashic@users.noreply.github.com>
2026-08-16 14:51:00 +0800

    Bump Openlist version to 4.2.5 and update checksums

--- Formula/openlist.rb
+++ Formula/openlist.rb
@@ -1,14 +1,14 @@
 class Openlist < Formula
   desc "New AList fork addressing anti-trust issues"
   homepage "https://doc.oplist.org/"
-  version "4.2.4"
+  version "4.2.5"
 
   if Hardware::CPU.arm?
     url "https://github.com/OpenListTeam/OpenList/releases/download/v#{version}/openlist-darwin-arm64.tar.gz"
-    sha256 "0e85d1c70aab2ba42f63fab407c7cffebc30d6cd573085e4b67365a3af6cb57f"
+    sha256 "95071c3b5b63ffc683bcb36b36d7f1359ba81d7650ec8e13336daa4124ec1cf6"
   else
     url "https://github.com/OpenListTeam/OpenList/releases/download/v#{version}/openlist-darwin-amd64.tar.gz"
-    sha256 "ead4d3230f8bb6e008e4475ec22590d1379dff3b9d8f25be7d57d0a04f6d3199"
+    sha256 "c3a9f0de6e0019f24108eaa20ad820d0e84b0ba4c857d27a49acc49a5f5e48e4"
   end
 
   def install
